51357c6b9be8d2b9fa933901c028b98f8250fce9
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-01-08 21:56:19 +0600

    Next.js project started

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main>
       <div>Hello world</div>
       <h1>Now I explore NextJS</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At quidem quod nulla rerum. Magni quas ipsam molestias sunt? Inventore ipsam perspiciatis harum dolor eum cum officiis ipsa repudiandae enim quia!</p>
     </main>
   )
 }
