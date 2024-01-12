eeec55ba7d37568e192d1b8a4173c4ede870988f
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-01-12 21:57:08 +0600

    Submit button added

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <div>Hello world</div>
       <h1>Now I explore NextJS</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At quidem quod nulla rerum. Magni quas ipsam molestias sunt? Inventore ipsam perspiciatis harum dolor eum cum officiis ipsa repudiandae enim quia!</p>
+      <button>Submit</button>
     </main>
   )
 }
